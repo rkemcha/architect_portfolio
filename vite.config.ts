035cd963844e35e2a7c29bc70cd4c0bb084d5713
rkemcha <150310398+rkemcha@users.noreply.github.com>
2026-07-10 14:04:43 +0200

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/architect_portfolio/', // Use relative paths for static hosting
+  base: './', // Use relative paths for static hosting
 })
